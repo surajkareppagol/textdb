f3de329827fec28fd09ff7c8fa71255ebb9866d8
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-02-05 17:45:04 +0530

    Initial code for TextDB
    - Add header files for utility and database functions

---
+++ include/db.h
@@ -0,0 +1,9 @@
+#ifndef DB_H
+#define DB_H
+
+extern char *database;
+
+void create_db(char *db);
+void create_table(char *table, char *members);
+
+#endif
---
+++ include/util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdbool.h>
+
+#define DATABASE_BASE "db"
+
+void util_create_dir(char *dir, bool base);
+
+#endif
