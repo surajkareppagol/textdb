ebce72d01ce4b40379583511f69854297b78007a
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-02-10 17:43:27 +0530

    Add db functions
    - Add add_values_table function
    - Add print_table function

--- src/db.c
+++ src/db.c
@@ -7,6 +7,11 @@
 
 /* Extern variable to hold database name */
 char *database = NULL;
+long int current_position = 0;
+
+/**********************************************************/
+/*                    CREATE_DB                           */
+/**********************************************************/
 
 void create_db(char *db) {
   util_create_dir(DATABASE_BASE, true);
@@ -17,9 +22,14 @@ void create_db(char *db) {
   strncpy(database, db, strlen(db));
 }
 
+/**********************************************************/
+/*                    CREATE_TABLE                        */
+/**********************************************************/
+
 void create_table(char *table, char *members) {
   char path[1024];
-  sprintf(path, "%s/%s/%s", DATABASE_BASE, database, table);
+  /* DATABASE_STORAGE / DATABASE / TABLE */
+  sprintf(path, "%s/%s/%s.txt", DATABASE_BASE, database, table);
 
   FILE *open_table = fopen(path, "w");
 
@@ -27,7 +37,86 @@ void create_table(char *table, char *members) {
     return;
   }
 
-  fprintf(open_table, "%s", members);
+  char *token = strtok(members, MEMBER_DELIMITER_COMMA);
+  fseek(open_table, current_position, SEEK_SET);
+
+  while (true) {
+    if (token == NULL) {
+      break;
+    }
+
+    token = util_trim(token);
+    util_create_label(token);
+    
+    fprintf(open_table, "%s ", token);
+    current_position = ftell(open_table);
+
+    token = strtok(NULL, MEMBER_DELIMITER_COMMA);
+  }
+
+  fprintf(open_table, "%s", DATABASE_NEW_LINE);
+
+  fclose(open_table);
+}
+
+/**********************************************************/
+/*                    ADD_VALUES_TABLE                    */
+/**********************************************************/
+
+void add_values_table(char *table, char *members) {
+  char path[1024];
+  /* DATABASE_STORAGE / DATABASE / TABLE */
+  sprintf(path, "%s/%s/%s.txt", DATABASE_BASE, database, table);
+
+  FILE *open_table = fopen(path, "a");
+
+  if (open_table == NULL) {
+    return;
+  }
+
+  char *token = strtok(members, MEMBER_DELIMITER_COMMA);
+  fseek(open_table, current_position, SEEK_SET);
+
+  while (true) {
+    if (token == NULL) {
+      break;
+    }
+
+    token = util_trim(token);
+    util_create_label(token);
+    
+    fprintf(open_table, "%s ", token);
+    current_position = ftell(open_table);
+
+    token = strtok(NULL, MEMBER_DELIMITER_COMMA);
+  }
+
+  fprintf(open_table, "%s", DATABASE_NEW_LINE);
+
+  fclose(open_table);
+}
+
+/**********************************************************/
+/*                    DB_PRINT_TABLE                         */
+/**********************************************************/
+
+void db_print_table(char *table)
+{
+  char path[1024];
+  /* DATABASE_STORAGE / DATABASE / TABLE */
+  sprintf(path, "%s/%s/%s.txt", DATABASE_BASE, database, table);
+
+  FILE *open_table = fopen(path, "r");
+
+  if (open_table == NULL) {
+    return;
+  }
+
+  char table_buffer[MAX_VALUE_BUFFER_SIZE] = {0};
+
+  while(fgets(table_buffer, MAX_VALUE_BUFFER_SIZE, open_table) != NULL) {
+    fprintf(stdout, "%s", table_buffer);
+  }
 
   fclose(open_table);
 }
